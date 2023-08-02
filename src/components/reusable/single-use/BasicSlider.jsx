c57393a5bff9c76feaf028231f698eea9d7613b4
VojinPopovic <vojinpopovic3@gmail.com>
2023-08-02 21:08:24 +0200

    style changes

--- src/components/reusable/single-use/BasicSlider.jsx
+++ src/components/reusable/single-use/BasicSlider.jsx
@@ -1,16 +1,13 @@
 import React, { useRef, useState } from "react";
-// Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import Image from "next/image";
 import Link from "next/link";
 import ImdbIcon from "../../../../public/imdbIcon.svg";
 
-// Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
-// import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function BasicSlider({ data }) {
