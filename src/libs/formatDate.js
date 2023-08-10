ec3d66d3bb56759745dd9e6e0b1a1a9645f6f7d5
VojinPopovic <vojinpopovic3@gmail.com>
2023-08-10 16:28:39 +0200

    vercel fix

--- src/libs/formatDate.js
+++ src/libs/formatDate.js
@@ -1,5 +1,5 @@
-export async function formatDate(dateString) {
-  const [year, month, day] = await dateString.split('-');
+export function formatDate(dateString) {
+  const [year, month, day] = dateString.split('-');
   const date = new Date(year, month - 1, day); // month is zero-based in Date constructor
   const options = { year: 'numeric', month: 'long', day: 'numeric' };
   return date.toLocaleDateString(undefined, options);
