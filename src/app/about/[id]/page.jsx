69d5d82ead7ac862a9ac26a9da4ef75f8a3d7eac
VojinPopovic <vojinpopovic3@gmail.com>
2023-08-11 15:51:34 +0200

    vercel fix

--- src/app/about/[id]/page.jsx
+++ src/app/about/[id]/page.jsx
@@ -5,10 +5,9 @@ import { getReviewsData } from "@/libs/getReviews";
 import AboutPageClient from "@/components/single-use/AboutPageClient";
 
 export default async function About({ params }) {
-  const movieNameYear = params.id.split("%2B");
-  const moviesData = getMovieBySearch(movieNameYear[0].replaceAll("%20", " "));
+  const moviesData = getMovieBySearch(params.id.split("%2B")[0].replaceAll("%20", " "));
   const popularityData = getPopularityData();
-  const reviews = await getReviewsData("moviename", movieNameYear[0]);
+  const reviews = await getReviewsData("moviename", params.id.split("%2B")[0]);
 
   const [movies, popularMovies] = await Promise.all([
     moviesData,
@@ -21,7 +20,7 @@ export default async function About({ params }) {
         reviews={reviews}
         movies={movies.results}
         popularMovies={popularMovies}
-        movieNameYear={movieNameYear}
+        movieNameYear={params.id.split("%2B")}
       />
     </>
   );
