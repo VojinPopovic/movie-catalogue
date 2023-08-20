7dc66429174902add1be5a04b20eeba6b217a0a2
VojinPopovic <vojinpopovic3@gmail.com>
2023-08-20 20:01:06 +0200

    finished watchlist page

--- src/app/watch-list/[id]/page.jsx
+++ src/app/watch-list/[id]/page.jsx
@@ -6,9 +6,18 @@ export default async function WatchList({ params }) {
     "useremail",
     decodeURI(params.id).replaceAll("%40", "@")
   );
+  console.log(watchlist);
 
-  return <div className="w-full max-w-[1048px] bg-white">{
-    // watchlist? 
+  const content = watchlist.map((movie) => {
+    return <MovieCard key={movie.id} movie={movie} />;
+  });
 
-  }</div>;
+  return (
+    <div className="text-white max-w-[1034px] mx-auto px-2">
+      <p className="text-white text-4xl my-5">Watchlist</p>
+      <div className="grid gap-3 xxs:grid-cols-[250px] xs:grid-cols-[200px_200px] sm:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px_200px_200px] content:grid-cols-[200px_200px_200px_200px_200px] content:gap-3 mx-auto pb-10">
+        {watchlist ? content : ""}
+      </div>
+    </div>
+  );
 }
