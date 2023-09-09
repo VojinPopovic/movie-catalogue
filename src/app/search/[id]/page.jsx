69e2b6e14d2edc964ab5e8b02df0d76d8194fe87
VojinPopovic <vojinpopovic3@gmail.com>
2023-09-09 16:53:43 +0200

    profile modal changes

--- src/app/search/[id]/page.jsx
+++ src/app/search/[id]/page.jsx
@@ -11,9 +11,13 @@ export default async function Search({ params }) {
   return (
     <div className="text-white w-full max-w-[1048px] flex flex-col justify-center">
       <p className="text-4xl text-center mt-5 mb-10">Search results</p>
-      <div className="grid gap-3 xxs:grid-cols-[250px] xs:grid-cols-[200px_200px] sm:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px_200px_200px] content:grid-cols-[200px_200px_200px_200px_200px] content:gap-3 mx-auto pb-10">
-        {content}
-      </div>
+      {searchResults?.results?.length >= 1 ? (
+        <div className="grid gap-3 xxs:grid-cols-[250px] xs:grid-cols-[200px_200px] sm:grid-cols-[200px_200px_200px] md:grid-cols-[200px_200px_200px_200px] content:grid-cols-[200px_200px_200px_200px_200px] content:gap-3 mx-auto pb-10">
+          {content}
+        </div>
+      ) : (
+        <p className="text-2xl text-center">No results</p>
+      )}
     </div>
   );
 }
